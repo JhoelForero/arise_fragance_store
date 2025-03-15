11ab9969386fbcb50bbf65f0bee6af18fd4b57cb
Jhoel Forero <jhoelforeroyate@gmail.com>
2025-03-15 18:39:16 -0400

    created php class structure to handle client requests

--- server/Application/application.php
+++ server/Application/application.php
@@ -1,5 +1,6 @@
 <?php
-class application {
+require_once '../Domain/domain.php';
+class application extends domain{ 
     /**this is the general class for the application, this will be extended to authentication service and catalog service. 
      * Ex: $myApp = new authentication(); //this contains both parent and child functionalities.
     */
