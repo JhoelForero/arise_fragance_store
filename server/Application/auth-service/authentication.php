211efea8ae4a20d26384d1eb942197436dd24168
Jhoel Forero <jhoelforeroyate@gmail.com>
2025-03-16 16:25:49 -0400

    arranged issues with directories in php

--- server/Application/auth-service/authentication.php
+++ server/Application/auth-service/authentication.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../application.php';
+require_once __DIR__ . '../application.php';
 class authentication extends application{
     //subclass for authentication requests: login, register, logout.
 }
